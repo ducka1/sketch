8c4218bb15363db3899b4ab2d9d8132d9d811d93
ducka1 <aparaduckadam@protonmail.com>
2022-05-29 17:20:41 +0300

    added button to clear and replace grid with the size provided

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 const container = document.querySelector("#container")
-let size = 16;
-
-for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) { 
-        container.appendChild(document.createElement("div")).classList.add("tile");
-    }
-}
-
-const tiles = document.querySelectorAll(".tile");
-let tileSize = 900 / size;
-
-tiles.forEach((tile) => {
-    tile.style.height = tileSize + "px";
-    tile.style.width = tileSize + "px";
-    tile.addEventListener("mouseover", () => {
-        tile.style.backgroundColor = "orangered";
-    });
-    // tile.addEventListener('mouseout', () => {
-    //     tile.style.backgroundColor = 'darkorange';
-    // });
-});
-
 const btn = document.querySelector("#btn");
 
 btn.addEventListener('click', () => {
-    alert('hello');
+    let size = parseInt(prompt("Enter the new size of the grid (under 101)", "16"));
+    if (size > 100) {
+        alert('The number you entered is too big');
+    } else {
+        container.replaceChildren() // remove grid
+        createGrid(size);
+    }
 });
+
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) { 
+            container.appendChild(document.createElement("div")).classList.add("tile");
+        }
+    }
+
+    let tiles = document.querySelectorAll(".tile");
+    let tileSize = 900 / size;
+
+    tiles.forEach((tile) => {
+        tile.style.height = tileSize + "px";
+        tile.style.width = tileSize + "px";
+        tile.addEventListener("mouseover", () => {
+            tile.style.backgroundColor = "orangered";
+        });
+    });
+}
+
+createGrid(16);
